3ff994eca95b935b163e5bc0011f17f0ec0ed4e0
Brian Kelly <brian@buckysolutions.com>
2026-07-06 15:47:27 -0400

    Fix Prisma client to use pg Pool with SSL for Supabase

--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "../../prisma/generated/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -19,7 +20,12 @@ function createPrismaClient(): PrismaClient {
     );
   }
 
-  const adapter = new PrismaPg({ connectionString });
+  const pool = new Pool({
+    connectionString,
+    ssl: { rejectUnauthorized: false },
+  });
+
+  const adapter = new PrismaPg(pool);
   return new PrismaClient({ adapter });
 }
 
